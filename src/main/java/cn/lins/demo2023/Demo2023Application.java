e70b65a89343b02ea9e9f1bacb4d42c8a7265804
lins <sssslin4@163.com>
2023-03-16 12:27:15 +0800

    demo-2023-3.16

--- src/main/java/cn/lins/demo2023/Demo2023Application.java
+++ src/main/java/cn/lins/demo2023/Demo2023Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2023Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Demo2023Application.class, args);
     }
