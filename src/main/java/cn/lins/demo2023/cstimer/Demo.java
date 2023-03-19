9d47a964662d8e5744dbaff46a453012f88660fb
lins <sssslin4@163.com>
2023-03-19 17:37:49 +0800

    Revert "demo-2023-3.16 2-3 bb z  da"
    This reverts commit 642602f4

--- src/main/java/cn/lins/demo2023/cstimer/Demo.java
+++ src/main/java/cn/lins/demo2023/cstimer/Demo.java
@@ -1,14 +1,11 @@
 package cn.lins.demo2023.cstimer;
 
-import org.apache.logging.log4j.util.Strings;
-
-import java.util.*;
+import java.util.Timer;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-
 public class Demo {
     public static void main(String[] args) {
-
+        System.out.println("s2211c");
     }
 }
